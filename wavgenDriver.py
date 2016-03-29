b4f1512798f85137409eb9a1e345ede68c1f0a74
David Hale <dhale@astro.caltech.edu>
2016-03-29 16:00:42 -0700

    updated the wavgenDriver.py script to accept input from command line dh

--- wavgenDriver.py
+++ wavgenDriver.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+# -----------------------------------------------------------------------------
+# @file     wavgenDriver.py
+# @brief    driver script for PHM.wavgen
+# @author   David Hale
+# @date     2016-xx-xx
+# @modified 2016-03-29 DH
+# 
+# This script invokes the PHM wavgen.
+# -----------------------------------------------------------------------------
+
 import fileinput
 import PHM.wavgen
 import matplotlib.pyplot as plt
@@ -8,18 +18,21 @@ sys.tracebacklimit=0
 # -----------------------------------------------------------------------------
 # @fn     main
 # @brief  
-# @param  sourceText
+# @param  source, name of the input .wdl file without the .wdl part
 # @return none
 # -----------------------------------------------------------------------------
-def main():
+def main(source):
     """
     """
-    PHM.wavgen.loadWDL("test.wdl", "test")
+    input  = source+".wdl"
+    output = source
+    PHM.wavgen.loadWDL(input, output)
     plt.show(block=True)
 
 # -----------------------------------------------------------------------------
 #           __main__
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
 
